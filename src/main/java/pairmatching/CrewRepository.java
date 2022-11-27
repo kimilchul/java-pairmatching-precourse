ca3f2fa750c39ef5348c267ba603481ddb94d9da
Ilchul Kim <huj0159@gmail.com>
2022-11-28 00:51:05 +0900

    feat: read file and return list

--- src/main/java/pairmatching/CrewRepository.java
+++ src/main/java/pairmatching/CrewRepository.java
@@ -1,2 +1,23 @@
-package pairmatching;public class CrewRepository {
+package pairmatching;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CrewRepository {
+
+    public List<String> getCrews(String course) {
+        List<String> crews = new LinkedList<>();
+        try (BufferedReader objReader = new BufferedReader(new FileReader("src/main/resources/" + course))) {
+            String currentLine;
+            while ((currentLine = objReader.readLine()) != null) {
+                crews.add(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return crews;
+    }
 }
