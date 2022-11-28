5913543cc7a49b01fdee6bf8935e99d5fa66ff4b
kimilchul <huj0159@gmail.com>
2022-11-28 09:54:41 +0900

    feat: shuffle list

--- src/main/java/pairmatching/PairMatching.java
+++ src/main/java/pairmatching/PairMatching.java
@@ -1,5 +1,27 @@
 package pairmatching;
 
-public class PairMatching {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
+public class PairMatching {
+    private Course course;
+    private Level level;
+    private Mission mission;
+
+    private List<String> crews;
+
+    CrewRepository crewRepository = new CrewRepository();
+
+    public PairMatching(PairMatchingDto dto){
+            this.course = dto.getCourse();
+            this.level = dto.getLevel();
+            this.mission = dto.getMission();
+
+            this.crews = crewRepository.getCrews(course.getFileName());
+    }
+
+    public List<String> getShuffledCrews(){
+        return Randoms.shuffle(this.crews);
+    }
 }
